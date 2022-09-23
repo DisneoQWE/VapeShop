c0f2d5b973a990be0290eec8a0f1f6fc9bf676c4
Sagindykov Arsan <disneoqwe@gmail.com>
2022-09-23 18:45:55 +0600

    0.0.1 Snapshot

---
+++ src/test/java/com/vapeshop/smoke/SmokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vapeshop.smoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
